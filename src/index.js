d6c97b4865b7a62c8590b581da7c9398f130aafb
Amir Hossein Kheirollahpour <amir1.kh2006@gmail.com>
2025-03-09 11:05:14 +0330

    eval set

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const input = document.getElementById("input")
+
+function addToInput(event){
+    input.value += event
+}
+function clearInput(){
+    input.value = ""
+}
+function backSpace(){
+    input.value = input.value.slice(0,-1)
+}
+function Calculate(){
+    try{
+        input.value = eval(input.value)
+    }
+    catch{
+        alert(خطا)
+        clearInput()
+    }
+}
